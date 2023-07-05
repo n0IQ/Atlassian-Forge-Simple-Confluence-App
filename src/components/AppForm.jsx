9ea2b96b7785e2687cd7884165819af7f99c7434
Mohit Varma <MohitVarma@Rahuls-MacBook-Pro.local>
2023-07-05 19:17:10 +0530

    Used REST API to implement Comments Count

--- src/components/AppForm.jsx
+++ src/components/AppForm.jsx
@@ -1,37 +1,45 @@
-import ForgeUI, { Fragment, useState, TextField, Form, Text, Button, useEffect } from '@forge/ui'
+import ForgeUI, {
+  Fragment,
+  useState,
+  TextField,
+  Form,
+  Text,
+  Button,
+  useEffect,
+} from '@forge/ui';
 
 const AppForm = () => {
-    const [formData, setFormData] = useState(null);
-    const [isFormFilled, setIsFormFilled] = useState(false);
+  const [formData, setFormData] = useState(null);
+  const [isFormFilled, setIsFormFilled] = useState(false);
 
-    const onSubmit = (data) => {
-      // console.log("Form Data", data);
-      setFormData(data);
-      setIsFormFilled(true);
-    }
+  const onSubmit = (data) => {
+    // console.log("Form Data", data);
+    setFormData(data);
+    setIsFormFilled(true);
+  };
 
-    const goBack = () => {
-      setFormData(null);
-      setIsFormFilled(false);
-    }
+  const goBack = () => {
+    setFormData(null);
+    setIsFormFilled(false);
+  };
 
   return (
     <Fragment>
       {isFormFilled ? (
         <Fragment>
           <Text>Hello, {formData && formData.name}</Text>
-          <Button onClick={goBack} text='Go Back'></Button>
+          <Button onClick={goBack} text="Go Back"></Button>
         </Fragment>
       ) : (
-        <Fragment> 
-          <Text>Simple Forge Form</Text>
+        <Fragment>
+          <Text>Forge Form</Text>
           <Form onSubmit={onSubmit}>
-           <TextField label='Name' name='name'/>
-         </Form>
-       </Fragment>
+            <TextField label="Name" name="name" />
+          </Form>
+        </Fragment>
       )}
     </Fragment>
-  )
-}
+  );
+};
 
-export default AppForm
+export default AppForm;
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,45 @@
-import ForgeUI, { render, Fragment, Macro, Text, Heading } from "@forge/ui";
-import AppForm from "./components/AppForm";
+import ForgeUI, {
+  render,
+  Fragment,
+  Macro,
+  Text,
+  Heading,
+  useProductContext,
+  useState,
+} from '@forge/ui';
+import api, { route } from '@forge/api';
+import AppForm from './components/AppForm';
+
+const fetchComments = async (contentId) => {
+  const res = await api
+    .asUser()
+    .requestConfluence(
+      route`/wiki/rest/api/content/${contentId}/child/comment`
+    );
+
+  const data = await res.json();
+  return data.results;
+};
 
 const App = () => {
+  const context = useProductContext();
+  const [comments] = useState(
+    async () => await fetchComments(context.contentId)
+  );
+
+  // console.log(`Number of comments on this page: ${comments.length}`);
+  // console.log('Comments', comments[0].body);
+
   return (
     <Fragment>
       <Heading>Forge App</Heading>
       <Text>Hello Everyone!</Text>
       <AppForm />
+      <Text>
+        Total Comments: {comments.length === undefined ? 0 : comments.length}
+      </Text>
     </Fragment>
   );
 };
 
-export const run = render(
-  <Macro
-    app={<App />}
-  />
-);
+export const run = render(<Macro app={<App />} />);
